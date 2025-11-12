a1ba40ea62ef018aadb1e10e2395576d9d67ec61
duerwuyi <1124457432@qq.com>
2025-11-12 17:38:48 +0800

    Add new error message for 'citus_schemas' in CitusCommon

--- src/sqlancer/citus/gen/CitusCommon.java
+++ src/sqlancer/citus/gen/CitusCommon.java
@@ -62,6 +62,7 @@ public final class CitusCommon {
         errors.add("Foreign keys and AFTER ROW triggers are not supported for columnar tables");
         errors.add(" ");
         errors.add("cannot define statistics for relation");
+        errors.add("citus_schemas");
 
         // current errors in Citus (to be removed once fixed)
         if (CitusBugs.bug3957) {
