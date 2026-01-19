0a74ae49b81094e092922fd1c904304f1b4d2e16
duerwuyi <78290899+duerwuyi@users.noreply.github.com>
2026-01-19 15:57:09 +0800

    Change SECONDS constant from 21000 to 600

--- test/sqlancer/dbms/TestConfig.java
+++ test/sqlancer/dbms/TestConfig.java
@@ -2,7 +2,7 @@ package sqlancer.dbms;
 
 public class TestConfig {
     public static final String NUM_QUERIES = "1000000";
-    public static final String SECONDS = "21000";
+    public static final String SECONDS = "600";
 
     public static final String CLICKHOUSE_ENV = "CLICKHOUSE_AVAILABLE";
     public static final String CNOSDB_ENV = "CNOSDB_AVAILABLE";
